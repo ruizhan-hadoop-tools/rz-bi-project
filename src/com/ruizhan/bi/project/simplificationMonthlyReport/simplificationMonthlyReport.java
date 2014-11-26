ca653679c0ba3b7f4b304812eb32cef9ab939f0a
ruizhan-qianchao <qianchao@game-reign.com>
2014-11-26 11:48:39 +0800

    empty

---
+++ src/com/ruizhan/bi/project/simplificationMonthlyReport/simplificationMonthlyReport.java
@@ -0,0 +1,10 @@
+package com.ruizhan.bi.project.simplificationMonthlyReport;
+
+public class simplificationMonthlyReport {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
